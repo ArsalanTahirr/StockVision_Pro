3759cf027f4ec5603746341e20e93f9d67f80296
Arsalan Tahir <88188911+ArsalanTahirr@users.noreply.github.com>
2026-01-14 22:55:06 +0500

    Remove Vercel deployment files

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Add project root to Python path so we can import app.py
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from app import app  # <-- imports your Flask instance
